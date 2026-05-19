632948b3affebcdc6372f8ccc359d65fde5a06e8
Garrett Bell <gt.bell20@gmail.com>
2026-05-18 22:54:12 -0500

    Implemented all functions in model.py except for 'performance_on_categorical_slice'

--- ml/model.py
+++ ml/model.py
@@ -2,6 +2,8 @@ import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
 # TODO: add necessary import
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import RandomizedSearchCV
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -19,8 +21,21 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
-    # TODO: implement the function
-    pass
+    # -----------------------------
+    # TODO: implement the function | DONE
+    # -----------------------------
+    # Simple hyperparameter distribution to test
+    param_dist = {
+        'n_estimators': range(50,100,10),
+        'max_depth': range(2,10,2)
+    }
+    # Create RandomForest instance and utilize RandomizedSearchCV to find optimal hyperparameter combination
+    rf_obj = RandomForestClassifier()
+    rf_cv = RandomizedSearchCV(rf_obj, param_dist, scoring='f1')
+    # Fit model to training set
+    rf_trained = rf_cv.fit(X_train, y_train)
+    # Return trained model
+    return rf_trained
 
 
 def compute_model_metrics(y, preds):
@@ -59,8 +74,11 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    # TODO: implement the function
-    pass
+    # -----------------------------
+    # TODO: implement the function | DONE
+    # -----------------------------
+    preds = model.predict(X)
+    return preds
 
 def save_model(model, path):
     """ Serializes model to a file.
@@ -72,13 +90,20 @@ def save_model(model, path):
     path : str
         Path to save pickle file.
     """
-    # TODO: implement the function
-    pass
+    # -----------------------------
+    # TODO: implement the function | DONE
+    # -----------------------------
+    with open(f'{path}.pkl', 'wb') as f:
+        pickle.dump(model, f)
 
 def load_model(path):
     """ Loads pickle file from `path` and returns it."""
-    # TODO: implement the function
-    pass
+    # -----------------------------
+    # TODO: implement the function | DONE
+    # -----------------------------
+    with open(f'{path}.pkl', 'rb') as f:
+        model = pickle.load(f)
+    return model
 
 
 def performance_on_categorical_slice(
@@ -117,7 +142,9 @@ def performance_on_categorical_slice(
     fbeta : float
 
     """
-    # TODO: implement the function
+    # -----------------------------
+    # TODO: implement the function |
+    # -----------------------------
     X_slice, y_slice, _, _ = process_data(
         # your code here
         # for input data, use data in column given as "column_name", with the slice_value 
